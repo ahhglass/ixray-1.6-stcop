4345a4dd7e5cc80b6cc56e545e15bddb0ca4108d
⊱ shunsui <prochanovangel@gmail.com>
2026-08-28 23:04:46 +0300

    feat(ui): улучшенное отображение имён при подборе предметов
    Порт Pickup Manager из feature/pickup-manager: XML-настройки стиля,
    плавное следование текста, группировка одинаковых предметов,
    подсказка клавиши [F] и строка состояния с градиентом.
    - SPickupTextStyle / LoadStylesFromXML из pickup_info.xml
    - проекция через Center(), lerp позиции, группировка по section
    - сохранён актуальный CanPickItem (ESPATIAL_TYPE::OBSTACLE)
    - добавлен gamedata/configs/ui/backend/pickup_info.xml

--- src/xrGame/PickupManager.cpp
+++ src/xrGame/PickupManager.cpp
@@ -4,6 +4,11 @@
 #include "inventory_item.h"
 #include "../xrEngine/GameMtlLib.h"
 #include "../xrEngine/CameraBase.h"
+#include "../xrUI/UIXmlInit.h"
+#include "../xrEngine/xr_level_controller.h"
+#include "../xrEngine/string_table.h"
+#include "../xrCore/_color.h"
+#include "Level.h"
 #define PICKUP_INFO_COLOR 0xFFDDDDDD
 
 CPickUpManager::CPickUpManager(CActor* NewOwner) :
@@ -11,46 +16,126 @@ CPickUpManager::CPickUpManager(CActor* NewOwner) :
 {
 	PickupMode = false;
 	PickupInfoRadius = 100;
+	_style.font = g_FontManager->pFontSystem;
+	_style.color = PICKUP_INFO_COLOR;
+	LoadStylesFromXML();
 }
 
 void CPickUpManager::RenderInfo()
 {
 	Owner->feel_touch_update(Owner->cam_FirstEye()->vPosition, PickupInfoRadius);
 
-	for (CObject* Item: Owner->feel_touch)
+	xr_vector<CObject*> visibleItems;
+	xr_set<u16> feelTouchIds;
+
+	for (CObject* Item : Owner->feel_touch)
 	{
-		if (CanPickItem(Render->ViewBase, Owner->cam_FirstEye()->vPosition, Item))
-			PickupInfoDraw(Item);
+		feelTouchIds.insert(Item->ID());
+
+		u16 objId = Item->ID();
+		Fvector2& smoothPos = _textPositions[objId];
+		Fvector2 targetPos = GetScreenPos(Item);
+
+		if (targetPos.x >= 0)
+		{
+			if (std::abs(smoothPos.x) < 1.0f && std::abs(smoothPos.y) < 1.0f)
+				smoothPos = targetPos;
+			else
+			{
+				float lerpFactor = _style.lerp_speed * Device.fTimeDelta * 60.0f;
+				clamp(lerpFactor, 0.0f, 1.0f);
+				smoothPos.x = _lerp(smoothPos.x, targetPos.x, lerpFactor);
+				smoothPos.y = _lerp(smoothPos.y, targetPos.y, lerpFactor);
+			}
+		}
+
+		if (Item->getVisible() && smoothPos.x >= 0 && CanPickItem(Render->ViewBase, Owner->cam_FirstEye()->vPosition, Item))
+			visibleItems.push_back(Item);
 	}
+
+	if (_style.group_same_items && !visibleItems.empty())
+	{
+		xr_vector<SPickupItemGroup> groups;
+		GroupItems(visibleItems, groups);
+		for (const auto& group : groups)
+			PickupInfoDraw(group.items[0], (u32)group.items.size());
+	}
+	else
+		for (CObject* item : visibleItems)
+			PickupInfoDraw(item);
+
+	for (auto it = _textPositions.begin(); it != _textPositions.end();)
+		feelTouchIds.find(it->first) == feelTouchIds.end() ? it = _textPositions.erase(it) : ++it;
 }
 
-void CPickUpManager::PickupInfoDraw(CObject* object)
+Fvector2 CPickUpManager::GetScreenPos(CObject* obj) const
+{
+	Fvector worldCenter;
+	obj->Center(worldCenter);
+
+	Fvector4 vRes;
+	Device.mFullTransform.transform(vRes, worldCenter);
+
+	if (vRes.z < 0 || vRes.w < 0 || vRes.x < -1.f || vRes.x > 1.f || vRes.y < -1.f || vRes.y > 1.f)
+		return Fvector2().set(-1.f, -1.f);
+
+	return Fvector2().set((1.f + vRes.x) / 2.f * Device.TargetWidth, (1.f - vRes.y) / 2.f * Device.TargetHeight);
+}
+
+void CPickUpManager::PickupInfoDraw(CObject* object, u32 item_count)
 {
 	CInventoryItem* item = object != nullptr ? object->cast_inventory_item() : nullptr;
 	if (!item)
 		return;
 
-	Fmatrix res;
-	res.mul(Device.mFullTransform, object->XFORM());
+	u16 objId = object->ID();
+	Fvector2& smoothPos = _textPositions[objId];
 
-	Fvector4 v_res;
-	Fvector shift;
-	res.transform(v_res, shift);
-
-	if (v_res.z < 0 || v_res.w < 0)
+	if (smoothPos.x < 0)
 		return;
 
-	if (v_res.x < -1.f || v_res.x > 1.f || v_res.y < -1.f || v_res.y > 1.f)
-		return;
+	CGameFont* font = _style.font ? _style.font : g_FontManager->pFontSystem;
+	font->SetAligment(_style.align_name);
+	font->SetColor(_style.color);
 
-	float x = (1.f + v_res.x) / 2.f * (Device.TargetWidth);
-	float y = (1.f - v_res.y) / 2.f * (Device.TargetHeight);
+	string512 text;
+	if (_style.show_key_binding)
+	{
+		LPCSTR keyName = GetKeyName();
+		if (keyName)
+		{
+			if (item_count > 1)
+				xr_sprintf(text, sizeof(text), "[%s] %s x%u", keyName, item->NameItem(), item_count);
+			else
+				xr_sprintf(text, sizeof(text), "[%s] %s", keyName, item->NameItem());
+		}
+		else
+			xr_sprintf(text, sizeof(text), item_count > 1 ? "%s x%u" : "%s", item->NameItem(), item_count);
+	}
+	else
+		xr_sprintf(text, sizeof(text), item_count > 1 ? "%s x%u" : "%s", item->NameItem(), item_count);
 
-	CGameFont* font = g_FontManager->pFontSystem;
-	font->SetAligment(CGameFont::alCenter);
-	font->SetColor(PICKUP_INFO_COLOR);
+	font->Out(smoothPos.x, smoothPos.y, text);
 
-	font->Out(x, y, item->NameItem());
+	if (_style.show_condition && item->IsUsingCondition())
+	{
+		float condition = item->GetCondition();
+		int percent = (int)(condition * 100.0f);
+
+		string256 conditionStr;
+		xr_sprintf(conditionStr, sizeof(conditionStr), "%d%%", percent);
+
+		shared_str conditionLabel = g_pStringTable->translate("ui_st_loot_info_condition");
+		string256 condText;
+		if (conditionLabel.size() > 0 && xr_strcmp(conditionLabel.c_str(), "ui_st_loot_info_condition") != 0)
+			xr_sprintf(condText, sizeof(condText), "%s: %s", conditionLabel.c_str(), conditionStr);
+		else
+			xr_sprintf(condText, sizeof(condText), "%s", conditionStr);
+
+		font->SetColor(GetConditionColor(condition));
+		font->SetAligment(_style.align_info);
+		font->Out(smoothPos.x, smoothPos.y + font->CurrentHeight_() * _style.line_spacing, "%s", condText);
+	}
 }
 
 #include "DestroyablePhysicsObject.h"
@@ -147,4 +232,123 @@ bool CPickUpManager::CanPickItem(const CFrustum& frustum, const Fvector& from, C
 		return false;
 
 	return !bOverlaped;
+}
+
+LPCSTR CPickUpManager::GetKeyName() const
+{
+	int dik = get_action_dik(kUSE);
+	return dik > 0 ? dik_to_keyname(dik) : nullptr;
+}
+
+u32 CPickUpManager::GetConditionColor(float condition) const
+{
+	clamp(condition, 0.0f, 1.0f);
+
+	Fcolor c1, c2, result;
+	c1.set(_style.condition_color_min);
+	c2.set(_style.condition_color_max);
+	result.lerp(c1, c2, condition);
+
+	return result.get();
+}
+
+void CPickUpManager::GroupItems(xr_vector<CObject*>& items, xr_vector<SPickupItemGroup>& out_groups) const
+{
+	out_groups.clear();
+	for (CObject* obj : items)
+	{
+		CInventoryItem* item = obj->cast_inventory_item();
+		if (!item)
+			continue;
+
+		shared_str section = item->m_section_id.size() ? item->m_section_id : obj->cNameSect();
+		Fvector2 pos = GetScreenPos(obj);
+		if (pos.x < 0)
+			continue;
+
+		bool found = false;
+		for (auto& group : out_groups)
+		{
+			if (group.section_name != section)
+				continue;
+			Fvector2 groupPos = GetScreenPos(group.items[0]);
+			if (groupPos.x >= 0 && pos.distance_to(groupPos) <= _style.group_distance)
+			{
+				group.items.push_back(obj);
+				found = true;
+				break;
+			}
+		}
+		if (!found)
+		{
+			SPickupItemGroup group;
+			group.section_name = section;
+			group.representative_id = obj->ID();
+			group.items.push_back(obj);
+			out_groups.push_back(group);
+		}
+	}
+}
+
+void CPickUpManager::LoadStylesFromXML()
+{
+	CUIXml uiXml;
+	if (!uiXml.Load(CONFIG_PATH, UI_PATH, "backend\\pickup_info.xml") || !uiXml.NavigateToNode("pickup_info", 0))
+		return;
+
+	LPCSTR path = "pickup_info";
+	u32 color = _style.color;
+	CGameFont* pFont = nullptr;
+	if (CUIXmlInit::InitFont(uiXml, path, 0, color, pFont) && pFont)
+	{
+		_style.font = pFont;
+		_style.color = color;
+	}
+
+	LPCSTR align = uiXml.ReadAttrib(path, 0, "align_name", nullptr);
+	if (align)
+	{
+		if (!xr_strcmp(align, "left"))
+			_style.align_name = CGameFont::alLeft;
+		else if (!xr_strcmp(align, "right"))
+			_style.align_name = CGameFont::alRight;
+		else
+			_style.align_name = CGameFont::alCenter;
+	}
+
+	align = uiXml.ReadAttrib(path, 0, "align_info", nullptr);
+	if (align)
+	{
+		if (!xr_strcmp(align, "left"))
+			_style.align_info = CGameFont::alLeft;
+		else if (!xr_strcmp(align, "right"))
+			_style.align_info = CGameFont::alRight;
+		else
+			_style.align_info = CGameFont::alCenter;
+	}
+
+	_style.lerp_speed = uiXml.ReadAttribFlt(path, 0, "lerp_speed", _style.lerp_speed);
+	_style.line_spacing = uiXml.ReadAttribFlt(path, 0, "line_spacing", _style.line_spacing);
+	_style.group_distance = uiXml.ReadAttribFlt(path, 0, "group_distance", _style.group_distance);
+	_style.show_key_binding = uiXml.ReadAttribInt(path, 0, "show_key", _style.show_key_binding) != 0;
+	_style.group_same_items = uiXml.ReadAttribInt(path, 0, "group_same_items", _style.group_same_items) != 0;
+	_style.show_condition = uiXml.ReadAttribInt(path, 0, "show_condition", _style.show_condition) != 0;
+
+	int r_min = uiXml.ReadAttribInt(path, 0, "condition_color_min_r", -1);
+	int g_min = uiXml.ReadAttribInt(path, 0, "condition_color_min_g", -1);
+	int b_min = uiXml.ReadAttribInt(path, 0, "condition_color_min_b", -1);
+	int a_min = uiXml.ReadAttribInt(path, 0, "condition_color_min_a", -1);
+	if (r_min >= 0 && g_min >= 0 && b_min >= 0 && a_min >= 0)
+		_style.condition_color_min = color_argb(a_min, r_min, g_min, b_min);
+
+	int r_max = uiXml.ReadAttribInt(path, 0, "condition_color_max_r", -1);
+	int g_max = uiXml.ReadAttribInt(path, 0, "condition_color_max_g", -1);
+	int b_max = uiXml.ReadAttribInt(path, 0, "condition_color_max_b", -1);
+	int a_max = uiXml.ReadAttribInt(path, 0, "condition_color_max_a", -1);
+	if (r_max >= 0 && g_max >= 0 && b_max >= 0 && a_max >= 0)
+		_style.condition_color_max = color_argb(a_max, r_max, g_max, b_max);
+
+	clamp(_style.lerp_speed, 0.0f, 1.0f);
+	clamp(_style.line_spacing, 0.5f, 3.0f);
+	clamp(_style.group_distance, 0.0f, 1000.0f);
 }
--- src/xrGame/PickupManager.h
+++ src/xrGame/PickupManager.h
@@ -1,21 +1,52 @@
 #pragma once
+#include "../xrCore/_stl_extensions.h"
+#include "../xrCore/_vector2.h"
+
 class CActor;
+class CUIXml;
+class CGameFont;
+class CObject;
+class CFrustum;
+
+struct SPickupItemGroup
+{
+	shared_str section_name;
+	xr_vector<CObject*> items;
+	u16 representative_id;
+
+	SPickupItemGroup() : representative_id(0) {}
+};
+
+struct SPickupTextStyle
+{
+	CGameFont* font;
+	u32 color;
+	CGameFont::EAligment align_name, align_info;
+	float lerp_speed, line_spacing, group_distance;
+	bool show_key_binding, group_same_items, show_condition;
+	u32 condition_color_min, condition_color_max;
+
+	SPickupTextStyle() : font(nullptr), color(0xFFDDDDDD), align_name(CGameFont::alCenter), align_info(CGameFont::alCenter),
+		lerp_speed(0.15f), line_spacing(1.2f), group_distance(50.0f),
+		show_key_binding(true), group_same_items(true), show_condition(false),
+		condition_color_min(0xFFFF0000), condition_color_max(0xFF00FF00) {}
+};
 
 class CPickUpManager
 {
 	CActor* Owner;
-
-	//расстояние подсветки предметов
 	float PickupInfoRadius;
-
-	//режим подбирания предметов
 	bool PickupMode;
 	collide::rq_results RQR;
+
+	xr_map<u16, Fvector2> _textPositions;
+	SPickupTextStyle _style;
+
 public:
 	CPickUpManager(CActor* Owner);
-	
-	void RenderInfo();
 
+	void RenderInfo();
+	void LoadStylesFromXML();
 	bool CanPickItem(const CFrustum& frustum, const Fvector& from, CObject* item);
 
 	IC void SetPickupRadius(float Radius) { PickupInfoRadius = Radius; }
@@ -23,5 +54,9 @@ public:
 	IC bool GetPickupMode() const { return PickupMode; }
 
 private:
-	void PickupInfoDraw(CObject* object);
+	void PickupInfoDraw(CObject* object, u32 item_count = 1);
+	Fvector2 GetScreenPos(CObject* obj) const;
+	LPCSTR GetKeyName() const;
+	u32 GetConditionColor(float condition) const;
+	void GroupItems(xr_vector<CObject*>& items, xr_vector<SPickupItemGroup>& out_groups) const;
 };
